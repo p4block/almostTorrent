7dbf336ac700ed060fbebcfed9e5c6515d7d9932
p4block <p4block@users.noreply.github.com>
2017-12-14 12:24:42 +0100

    More setup, example of inter-package calling.

--- src/almostTorrent/client/clientMain.java
+++ src/almostTorrent/client/clientMain.java
@@ -1,8 +1,11 @@
 package almostTorrent.client;
 
+import almostTorrent.utils.randomUtils;
+
 public class clientMain {
 
     public static void main(String[] args){
         System.out.println(args[0]);
+        System.out.println(randomUtils.stupidHelper(3));
     }
 }
--- src/almostTorrent/utils/randomUtils.java
+++ src/almostTorrent/utils/randomUtils.java
@@ -1,4 +1,8 @@
 package almostTorrent.utils;
 
 public class randomUtils {
+
+    public static int stupidHelper(int i){
+        return i*2;
+    }
 }
