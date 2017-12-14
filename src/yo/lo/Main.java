78553499243c029691722d581fb4fc587ccc4401
p4block <p4block@users.noreply.github.com>
2017-12-14 11:53:43 +0100

    Testing

--- src/yo/lo/Main.java
+++ src/yo/lo/Main.java
@@ -4,13 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("HelloWorld");
-        System.out.println("HelloWorld");
-        System.out.println("HelloWorld");
-        System.out.println("HelloWorld");
-        System.out.println("HelloWorld");
-        System.out.println("HelloWorld");
-        System.out.println("HelloWorld");
-        System.out.println("HelloWorld");
-        System.out.println("HelloWorld");
+        System.out.println("Hello Guys");
+
     }
 }
