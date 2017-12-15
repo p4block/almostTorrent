e66d579553a14fce7bb7658bc1cc89d8fafae2d9
p4block <p4block@users.noreply.github.com>
2017-12-15 17:11:03 +0100

    We continue on

--- src/almostTorrent/starter.java
+++ src/almostTorrent/starter.java
@@ -8,20 +8,27 @@ import static almostTorrent.utils.randomUtils.*;
 
 public class starter {
 
-    public static void main(String[] args) {
+    private static String[] mArgs;
 
+    public static void main(String[] args) {
+        mArgs = args;
+
+        initializeStarter();
+    }
+
+    private static void initializeStarter(){
         // Start all
         randomUtils.initializeHelpers();
 
         ep("\n=== almostTorrent CLI launcher ===");
 
-        for (String i : args) {
+        for (String i : mArgs) {
             switch (i) {
                 case "-c":
-                    clientMain.main(args);
+                    startClient();
                     break;
                 case "-s":
-                    serverMain.main(args);
+                    startServer();
                     break;
                 case "-i":
                     startCliLoop();
@@ -35,32 +42,39 @@ public class starter {
 
     private static void startCliLoop() {
         ep("Entering almostTorrent master shell");
+        randomUtils.printHelp("masterShell");
 
         while (true) {
+            System.out.print("\n % ");
+
             switch (mKbScanner.nextLine().toLowerCase()) {
+                case "start client":
+                    startClient();
+                    break;
+                case "start server":
+                    startServer();
+                    break;
+                case "exit":
+                    System.exit(0);
                 case "help":
-                    randomUtils.printHelp("shell");
+                    randomUtils.printHelp("masterShell");
                     break;
                 case "?":
-                    randomUtils.printHelp("shell");
+                    randomUtils.printHelp("masterShell");
                     break;
-//                case "tracker":
-//                    if (mKbScanner.nextLine().equals("set")) {
-//                        ep("Set tracker now");
-//                        break;
-//                    } else if (mKbScanner.nextLine().equals("get")) {
-//                        ep("Get tracker");
-//                        break;
-//                    } else {
-//                        ep("Invalid command. Use help or ?.");
-//                        break;
-//                    }
-                case "tracker set":
-                    ep("Set tracker");
-//                default:
-//                    printHelp("shell");
+                default:
+                    printHelp("masterShell");
             }
+
         }
 
+    }
+
+    private static void startClient(){
+        clientMain.main(mArgs);
+    }
+
+    private static void startServer(){
+        serverMain.main(mArgs);
     }
 }
--- src/almostTorrent/utils/randomUtils.java
+++ src/almostTorrent/utils/randomUtils.java
@@ -5,21 +5,25 @@ import java.util.Scanner;
 public class randomUtils {
 
     // Help helper
-    public static void printHelp(String input) {
-        switch (input) {
+    public static void printHelp(String topic) {
+        switch (topic) {
             case "jar":
                 ep("java -jar randomUtils.jar -s/-c/-i");
                 ep("-s server \n-c for client \n-i for interactive shell");
                 break;
-            case "shell":
-                ep("*** AlmostTorrent CLI Help ***");
-                ep("--> tracker set <IP>:<Port>");
-                ep("--> tracker get");
-                ep("--> share file <Path>");
-                ep("--> share dir <Path>");
-                ep("--> show peers");
-                ep("--> mode set <Mode>");
-                ep("--> mode get");
+            case "masterShell":
+                ep("Available commands:");
+                ep("start (client || server)");
+                ep("exit");
+                ep("help");
+
+//                ep("--> tracker set <IP>:<Port>");
+//                ep("--> tracker get");
+//                ep("--> share file <Path>");
+//                ep("--> share dir <Path>");
+//                ep("--> show peers");
+//                ep("--> mode set <Mode>");
+//                ep("--> mode get");
                 break;
         }
     }
